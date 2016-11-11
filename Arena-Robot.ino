7b7041df7d75d0f3122f9d1f03e74597045a763a
DanJBarry <dbarry@stevens.edu>
2016-11-10 21:01:22 -0500

    Added beacon light subsystem
    Also improved movement of the robot, which also conveniently improved
    the flow of the code in general

--- Arena-Robot.ino
+++ Arena-Robot.ino
@@ -4,15 +4,17 @@
 //motor 1 is left
 //motor 2 is right
 
-unsigned int voltage;
+unsigned int floorSensor;
 unsigned int leftSensor;
 unsigned int frontSensor;
 unsigned int rightSensor;
+unsigned int beaconSensor;
 char base;
 boolean onWhite;
-boolean change = false;
+boolean change = false; //has the color of the floor changed since the last reading?
 float coords[2]; // x coord = 0, y coord = 1;
-
+char motorStatus = 'x'; // indicates which way the robot is currently going; x == stopped, w == forward, a == left, s == backward, d == right
+unsigned long startTime;
 
 void setup() {
   configArduino();
@@ -20,103 +22,149 @@ void setup() {
   attachInterrupt(1, rightBumper, LOW);   
   outputHigh(4);
   pause(100);
-  voltage = readADC(0);
-  if (voltage <= 19000) base = 'w';
-  if (voltage > 19000) base = 'b';
-  pause(100);
+  floorSensor = readADC(0);
+  if (floorSensor <= 19000) base = 'w'; //home is white
+  if (floorSensor > 19000) base = 'b'; //home is black
 }
 
 void loop() {
-  if (isHome() == true) beacon();
-  else enemy();
+  if (isHome() == true) { //if we're home, seek beacon light
+    outputHigh(12);
+    outputLow(10);
+    beacon();
+  }
+  else  { //if we're not home, seek enemy light
+    outputHigh(10);
+    outputLow(12);
+    enemy();
+  }
+}
+
+void halt() {
+  motors('b', 'o', 0);
+  motorStatus = 'x';
+  return;
 }
 
 void forward() {
-  pause(100);
-  motors('1', 'a', 50);
-  motors('2', 'a', 60);
+  if (motorStatus != 'w') {
+    if (motorStatus = 'a') motors('1', 'o', 0); //only stops motor(s) not turning the desired direction
+    if (motorStatus = 's') motors('b', 'o', 0);
+    if (motorStatus = 'd') motors('2', 'o', 0);
+    if (motorStatus != 'x') pause(100); //the pause is unneccessary if the motors are stopped
+    motors('1', 'a', 50);
+    motors('2', 'a', 60);
+    motorStatus = 'w';
+  }
   return;
 }
 
 void back() {
-  pause(100);
-  motors('1', 'b', 50);
-  motors('2', 'b', 60);
+  if (motorStatus != 's') {
+    if (motorStatus = 'w') motors('b', 'o', 0);
+    if (motorStatus = 'a') motors('2', 'o', 0);
+    if (motorStatus = 'd') motors('1', 'o', 0);
+    if (motorStatus != 'x') pause(100);
+    motors('1', 'b', 50);
+    motors('2', 'b', 60);
+    motorStatus = 's';
+  }
   return;
 }
 
 void left() {
-  pause(100);
-  motors('1', 'a', 50);
-  motors('2', 'b', 60);
+  if (motorStatus != 'a') {
+    if (motorStatus = 'w') motors('1', 'o', 0);
+    if (motorStatus = 's') motors('2', 'o', 0);
+    if (motorStatus = 'd') motors('b', 'o', 0);
+    if (motorStatus != 'x') pause(100);
+    motors('1', 'a', 50);
+    motors('2', 'b', 60);
+    motorStatus = 'a';
+  }
   return;
 }
 
 void right() {
-  pause(100);
-  motors('1', 'b', 50);
-  motors('2', 'a', 60);
+  if (motorStatus != 'd') {
+    if (motorStatus = 'w') motors('2', 'o', 0);
+    if (motorStatus = 'a') motors('b', 'o', 0);
+    if (motorStatus = 's') motors('1', 'o', 0);
+    if (motorStatus != 'x') pause(100);
+    motors('1', 'b', 50);
+    motors('2', 'a', 60);
+    motorStatus = 'd';
+  }
   return;
 }
 
 void forwardD(float distance){
-  pause(100);
-  motors('1', 'a', 50);
-  motors('2', 'a', 60);
+  forward();
   pause(distance * (1 / .009093));
-  motors('b', 'o', 0);
+  halt();
   return;
 }
 
 void backD(float distance) {
-  pause(100);
-  motors('1', 'b', 50);
-  motors('2', 'b', 60);
+  back();
   pause(distance * (1 / .009093));
-  motors('b', 'o', 0);
+  halt();
   return;
 }
 
 void leftD(float degree) {
-  pause(100);
-  motors('1', 'a', 50);
-  motors('2', 'b', 60);
+  left();
   pause(degree * 3.8);
-  motors('b', 'o', 0);
+  halt();
   return;
 }
 
 void rightD(float degree) {
-  pause(100);
-  motors('1', 'b', 50);
-  motors('2', 'a', 60);
+  right();
   pause(degree * 3.75);
-  motors('b', 'o', 0);
+  halt();
   return;
 }
 
 boolean isHome() { //tests if the robot is currently on friendly ground
-  voltage = readADC(0);
-  if (voltage <= 20000) {
-    onWhite = true;
-  }
-  if (voltage > 20000) {
-    onWhite = false;
-  }
-  if (base == 'w') {
-    if (onWhite == true) return true;
+  floorSensor = readADC(0);
+  if (floorSensor <= 20000) { //currently on white
+    if (base == 'w') return true;
     else return false;
   }
-  if (base == 'b') {
-    if (onWhite == false) return true;
+  else { //currently on black
+    if (base == 'b') return true;
     else return false;
   }
-  motors('b', 'o', 0);
 }
 
 void beacon() {
-  forward();
-    while (isHome() == true) forward();
+  int beaconInit, beaconFinal;
+  beaconInit = readADC(4);
+  leftD(30);
+  beaconFinal = readADC(4);
+  if (beaconFinal - beaconInit > 0) { // if the beacon light increased, keep turning left
+    left();
+    startTime = millis();
+    while (beaconSensor < 18000) { //turn until within a certain range of the beacon light
+      if (millis() - startTime >= 684) { //if the robot has made a full rotation without exiting, something went wrong, go forward a bit and start over
+        forwardD(.25);
+        break;
+      }
+      beaconSensor = readADC(4);
+    }
+  }
+  else {
+    right();
+    startTime = millis();
+    while (beaconSensor < 18000) { //same as above, except in the opposite direction
+      if (millis() -startTime >= 684) {
+        forwardD(.25);
+        break;
+      }
+      beaconSensor = readADC(4);
+    }
+  }
   return;
 }
 
@@ -124,14 +172,17 @@ void enemy() {
   leftSensor = readADC(3) - 300;
   frontSensor = readADC(2);
   rightSensor = readADC(1) + 500;
-  if (leftSensor < 16000 || frontSensor < 18000 || rightSensor < 16000) { //seeks out a light (just goes forward for now) until it gets within a certain range of a light
-    if (frontSensor <= leftSensor && frontSensor <= rightSensor) forward(); //if the front sensor is receiving the most light, go forward
+  if (leftSensor < 16000 || frontSensor < 18000 || rightSensor < 16000) { //if we are close to a light, seek it out
+    if ((frontSensor >= leftSensor && frontSensor >= rightSensor) || abs(frontSensor - leftSensor) <= 500 || abs(frontSensor - leftSensor) <= 500) forward(); //if the front sensor is receiving the most light, or very similar to another sensor, go forward
     else {
-      motors('b', 'o', 0);
-      pause(100);
+      startTime = millis();
       if (leftSensor <= rightSensor) { //if the left sensor is stronger than the right, turn left until the front sensor is stronger
         left();
         while (leftSensor < frontSensor) {
+          if (millis() - startTime >= 684) {
+            forwardD(.25);
+            break;
+          }
           leftSensor = readADC(3) - 300;
           frontSensor = readADC(2);
           rightSensor = readADC(1) + 500;
@@ -139,31 +190,22 @@ void enemy() {
       }
       else { //if the left sensor is not stronger than the right, turn right until the front sensor is stronger than the right one
         right();
+        
         while (rightSensor < frontSensor) {
+          if (millis() - startTime >= 684) {
+            forwardD(.25);
+            break;
+          }
           leftSensor = readADC(3) - 300;
           frontSensor = readADC(2);
           rightSensor = readADC(1) + 500;
         }
       }
-      pause(100); 
-      motors('b', 'o', 0);
-      pause(100);
     }
   }
   else forward(); //probably should replace this with something more complicated and productive in the future
   return;
 }
-
-/*
-  if (leftSensor >= rightSensor) {
-      if (lightPos(1) >= 0) leftD((180 * PI) * atan(lightPos(1) / lightPos(2)));
-      else leftD(90 + (180 * PI) * atan(lightPos(1) / lightPos(2)));
-    }
-    else {
-      if (lightPos(1) >= 0) rightD((180 * PI) * atan(lightPos(1) / lightPos(2)));
-      else rightD(90 + (180 * PI) * atan(lightPos(1) / lightPos(2)));
-    }
-*/
 
 //x coordinate = coords[0]
 //y coordinate = coords[1] 
@@ -178,13 +220,9 @@ float lightPos(int x) { //gives approximate position of the closest light, assum
 }
 
 void leftBumper() { //replace with something more complicated in the future
-  pause(5);
-  motors('b', 'o', 0);
   backD(.25);
-  pause(100);
   rightD(45);
-  pause(100);
-  forward();
+  forwardD(.25);
   return;
   
   /*
@@ -205,13 +243,9 @@ void leftBumper() { //replace with something more complicated in the future
 }
 
 void rightBumper() { //see leftBumper
-  pause(5);
-  motors('b', 'o', 0);
   backD(.25);
-  pause(100);
   leftD(45);
-  pause(100);
-  forward();
+  forwardD(.25);
   return;
 }
 
